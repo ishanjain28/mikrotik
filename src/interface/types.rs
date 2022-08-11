9a2b32e9065ebd8f444473d288e833b2aab4d8f9
Ishan Jain <contact@ishanjain.me>
2022-08-11 13:33:57 +0530

    derive Hash trait on types

--- src/interface/types.rs
+++ src/interface/types.rs
@@ -6,7 +6,7 @@ use serde::{
     Deserialize, Deserializer, Serialize,
 };
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Hash, Clone, PartialEq, Serialize, Deserialize)]
 #[non_exhaustive]
 pub enum InterfaceType {
     #[serde(rename = "vlan")]
@@ -27,7 +27,7 @@ impl Default for InterfaceType {
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Hash, Clone, PartialEq, Serialize, Deserialize)]
 #[non_exhaustive]
 pub enum Mtu {
     #[serde(rename = "auto")]
@@ -57,7 +57,7 @@ where
     }
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Default, Hash, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Interface {
     #[serde(rename = ".id")]
@@ -121,7 +121,7 @@ pub struct Interface {
     pub last_link_down_time: Option<String>,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Default, Hash, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct WireguardInterface {
     #[serde(rename = ".id")]
--- src/system/types.rs
+++ src/system/types.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
 use serde::Serialize;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Default, Hash, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Health {
     #[serde(rename = ".id")]
